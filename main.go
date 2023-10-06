7a36aed40cfc91322434a1e2362f8c865cc4154f
Austin Hunt <austin@hailtrace.com>
2023-10-06 14:26:34 -0400

    feat: make CANOPY_API_KEY required to run

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "canopyweather.com/canopy-cli/cmd"
+import (
+	"log"
+	"os"
+
+	"canopyweather.com/canopy-cli/cmd"
+)
 
 func main() {
+	if os.Getenv("CANOPY_API_KEY") == "" {
+		log.Fatal("CANOPY_API_KEY must be set to a valid api key")
+	}
+
 	cmd.Execute()
 }
